96ff2437ac67fc9fe5ec1edcf5d778f3927bb465
Jim Gumbley <jim.gumbley@stockport.gov.uk>
2017-01-06 13:23:30 +0000

    proxy working in windows under docker compose

---
+++ go.cmd
@@ -0,0 +1,18 @@
+@echo off
+echo This will drop and recreate your local docker environment and rebuild with fake data.  Are you sure?
+set INPUT=
+set /P INPUT=Type 'yes': %=%
+If /I "%INPUT%"=="yes" goto meat
+echo Cancelled
+goto end
+:meat
+
+cd infra
+echo docker-compose down --rmi all
+docker-compose up -d
+cd ../ingest
+lein run -m ingest.utils.bootstrap-demo
+cd ..
+
+
+:end
