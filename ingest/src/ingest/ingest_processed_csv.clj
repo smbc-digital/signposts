563dba6741307f617e6bdf686ca206b5712b4b80
Richard Filippi <richard@rogerthemoose.co.uk>
2017-01-12 10:49:35 +0000

    fix quick ingester for real data to use fixed input format for dates and to be lazy when reading large files

--- ingest/src/ingest/ingest_processed_csv.clj
+++ ingest/src/ingest/ingest_processed_csv.clj
@@ -1,25 +1,33 @@
 (ns ingest.ingest-processed-csv
   (:require [clojure-csv.core :as cs]
             [ingest.client.elastic-search-client :as esc]
-            [clj-time.format :as f]))
+            [clj-time.format :as f]
+            [clojure.java.io :as io]))
+
 
 (defn raw-events [file]
-  (let [csv (cs/parse-csv (slurp file))
+  (let [csv (cs/parse-csv (io/reader file))
         keys (map keyword (filter not-empty (first csv)))]
     (map #(zipmap keys (filter (fn [x] (not (nil? x))) %)) (rest csv))))
 
+(def input-date-format (f/formatter "dd/MM/yyyy"))
+
 (defn date-fmt [field format]
   (fn [event]
-    (assoc event field (f/unparse (format f/formatters) (f/parse (field event))))))
+    (if (field event)
+    (assoc event field (f/unparse format (f/parse input-date-format (field event))))
+    event
+    )))
 
-(def clean-dob (date-fmt :dob :date))
-(def clean-ts (date-fmt :timestamp :date-time))
+(def clean-dob (date-fmt :dob (:date f/formatters)))
+(def clean-ts (date-fmt :timestamp (:date-time f/formatters)))
 
 (defn ingest [file]
   (map
     #(-> %
          clean-dob
-         clean-ts)
+         clean-ts
+         )
     (raw-events file)))
 
 (defn upload [file]
