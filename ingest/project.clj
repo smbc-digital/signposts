1ee199b4d4a3d075911c563e33f5d36a4827d9c5
Richard Filippi <richard@rogerthemoose.co.uk>
2017-02-03 09:17:23 +0000

    rich #80 - add main and fix uberjar target

--- ingest/project.clj
+++ ingest/project.clj
@@ -11,11 +11,14 @@
                  [clojure-future-spec "1.9.0-alpha14"]
                  [camel-snake-kebab "0.4.0"]
                  [clj-http "2.3.0"]
+                 [clj-time "0.13.0"]
                  [me.raynes/fs "1.4.6"]
                  [base64-clj "0.1.1"]
                  [pandect "0.6.1"]
                  [semantic-csv "0.1.0"]
                  [levand/immuconf "0.1.0"]]
+
+  :main gov.stockport.sonar.ingest
 
   :test-paths ["test/utils" "test/unit" "test/integration"]
 
@@ -25,5 +28,6 @@
                            :dependencies [[midje "1.8.3"]
                                           [faker "0.2.2"]]}
              :unit        {:test-paths ^:replace ["test/unit" "test/utils"]}
-             :integration {:test-paths ^:replace ["test/integration" "test/utils"]}})
+             :integration {:test-paths ^:replace ["test/integration" "test/utils"]}
+             :uberjar     {:aot :all}})
 
--- ingest/src/gov/stockport/sonar/ingest.clj
+++ ingest/src/gov/stockport/sonar/ingest.clj
@@ -1,7 +1,5 @@
 (ns gov.stockport.sonar.ingest
   (:require [gov.stockport.sonar.ingest.inbound.feeds :as feeds]
-            [gov.stockport.sonar.ingest.inbound.flusher :as flusher]
-            [gov.stockport.sonar.ingest.inbound.event-buffer :as buffer]
             [gov.stockport.sonar.ingest.inbound.pipeline :refer [process-event-data]])
   (:gen-class))
 
