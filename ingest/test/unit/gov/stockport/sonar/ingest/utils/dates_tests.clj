11003dfe394a377c0b58b17c22eb6cf480b34104
Richard Filippi <richard@rogerthemoose.co.uk>
2017-02-10 10:50:18 +0000

    rich #75 - make the test check the benchmark timings

--- ingest/test/unit/gov/stockport/sonar/ingest/utils/dates_tests.clj
+++ ingest/test/unit/gov/stockport/sonar/ingest/utils/dates_tests.clj
@@ -10,8 +10,8 @@
 (defn parses? [val]
   (fn [fmt]
     (let [result (try (f/parse fmt val) (catch Exception _ nil))]
-    (or (= result expected-date)
-      (checking/as-data-laden-falsehood {:notes [(str "Failed for " val " produced [" result "]")]})))))
+      (or (= result expected-date)
+          (checking/as-data-laden-falsehood {:notes [(str "Failed for " val " produced [" result "]")]})))))
 
 (fact "should return matching format for date"
       (d/format-for "1995-11-19") => (parses? "1995-11-19")
@@ -25,11 +25,31 @@
       (d/format-for "19-nov-1995") => (parses? "19-nov-1995")
       (d/format-for "19-NOV-1995") => (parses? "19-NOV-1995"))
 
+(defmacro bench
+  "Times the execution of forms, discarding their output and returning
+a long in nanoseconds."
+  ([& forms]
+   `(let [start# (System/nanoTime)]
+      ~@forms
+      (- (System/nanoTime) start#))))
+
+(def form-1 "1995-11-19 12:12:12")
+(def form-2 "1995-11-19T12:12:12")
+
+(defn time-parser [parse-fn input]
+  (let [nanos (bench (count (map parse-fn (take 500 input))))]
+    (println (/ nanos 1000000.0))
+    nanos))
+
 (defn time-parser-against-single-format [parse-fn]
-  (time (count (map parse-fn (take 500 (repeat "1995-11-19 12:12:12"))))))
+  (time-parser parse-fn (repeat form-1)))
 
 (defn time-parser-against-multiple-formats [parse-fn]
-  (time (count (map parse-fn (take 500 (interleave (repeat "1995-11-19T12:12:12") (repeat "1995-11-19 12:12:12")))))))
+  (time-parser parse-fn (interleave (repeat form-2) (repeat form-1))))
+
+(fact "should parse test forms"
+      (d/parse form-1) => (t/date-time 1995 11 19 12 12 12)
+      (d/parse form-2) => (t/date-time 1995 11 19 12 12 12))
 
 (fact "should parse dates efficiently when similar format presented"
       (< (time-parser-against-single-format d/parse) (time-parser-against-single-format f/parse)))
