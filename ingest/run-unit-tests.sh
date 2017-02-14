1efc9443e7fd7121ad0c80648b66402ed522f16c
ZeshanRasul <zeshanrasul90@gmail.com>
2017-02-14 15:50:07 +0000

    Docker build runs unit tests before running ingester

--- ingest/run-unit-tests.sh
+++ ingest/run-unit-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "will now run the midje unit tests; if you supply ':autotest' as a parameter then you'll get a continuous test runner"
-lein clean && lein with-profile +unit midje $* 
+./lein clean && ./lein with-profile +unit midje $* 
