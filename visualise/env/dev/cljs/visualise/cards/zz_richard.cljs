6693e3c5320a91b20c0373895011bb8cc698505b
Richard Filippi <richard@rogerthemoose.co.uk>
2017-02-17 15:59:14 +0000

    rich / zeshan - just adding some docs

--- visualise/env/dev/cljs/visualise/cards/zz_richard.cljs
+++ visualise/env/dev/cljs/visualise/cards/zz_richard.cljs
@@ -12,8 +12,51 @@
 
 (defcard-doc
   "
-  ###Richard's Scratchpad
+  ###Dynamic Sub-Components
+
+  subcomp is one one of our traditional reagent components that produces an input box with an id of some description
+
+  ```
+  (defn subcomp [!state id]
+    (let [getter (fn [] (:val (get-in @!state (path id))))]
+      ; this ^{} attaches a metadata map to the hiccup list which can be
+      ; accessed programmatically if you have a reference to the list
+      ; in this case we are providing a 'getter' that can return the data
+      ; from the input field
+      ^{:getter getter}
+      [:div
+       [:label (str id \" \")
+        [:input
+         {:type      :text
+          :value     (getter)
+          :on-change #(swap! !state assoc-in (path id) {:val (-> % .-target .-value)})}]]]))
+  ```
+
+  subcomps is just a function that maps over subcomp and builds n (defined in !state) of the input subcomp components
+
+  the controller renders a div containing the list produced by subcomps
+
+  ```
+  (defn controller [!state]
+    (let [subcomps (subcomps !state)]
+      `[:div
+        ~@subcomps
+        ~[:input {:type     :submit
+                  :value    \"PANIC!!!\"
+                  :on-click (fn [] (do-something-with subcomps))}]]))
+  ```
+
+  the syntax quoting is the interesting bit here...
+
+  ```
+  ; essentially the syntax ` and ~@ is needed to allow the controller
+  ; to return a [:div [:div] [:div] ... ]
+  ; rather than [:div [ [:div] [:div] ... ] ] that you would get without it
+  ; it's subtle ask Zeshan for more info :-)
+  ```
   ")
+
+(defonce !state (atom {:comps 5}))
 
 (defn path [id]
   [:search-control :query-fields (keyword (str "subcomp-" id))])
@@ -31,14 +74,15 @@
 (defn subcomps [!state]
   (doall (map #(subcomp !state %) (range 1 (+ 1 (:comps @!state))))))
 
-(defonce !state (atom {:comps 5}))
-
 (defn do-something-with [subcomps]
   (doall (map (fn [subcomp] (println ((:getter (meta subcomp))))) subcomps)))
 
 (defn controller [!state]
   (let [subcomps (subcomps !state)]
     `[:div
+      ~[:label "number of sub-components " [:input {:type  :text
+                                                   :value (:comps @!state)
+                                                    :on-change #(swap! !state assoc :comps (-> % .-target .-value int))}]]
       ~@subcomps
       ~[:input {:type     :submit
                 :value    "PANIC!!!"
