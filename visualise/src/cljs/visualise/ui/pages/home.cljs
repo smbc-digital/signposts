02829488e27bc7d7e9fb0fe2a489d587dd9f93ca
ZeshanRasul <zeshanrasul90@gmail.com>
2017-02-21 11:54:52 +0000

    Added playground styling to home page

--- visualise/src/cljs/visualise/ui/pages/home.cljs
+++ visualise/src/cljs/visualise/ui/pages/home.cljs
@@ -8,19 +8,33 @@
 (defonce !data (r/atom {}))
 
 (defn home-page []
-  [:div.container-fluid
-   [:nav
-    [:div.col-sm-12
-     [:h3 "Stockport | SoNAR"]]]
-   [:div.col-sm-4
-    [:div.panel.panel-default
-     [:div.panel-heading
-      [:div.panel-title "search by"]]
-     [:div.panel-body
-      [sc/search-control !app (h/default-handler !data)]]]]
-   [:div.col-sm-8
-    [:div.panel.panel-default
-     [:div.panel-heading
-      [:div.panel-title "results"]]
-     [:div.panel-body
-      [rt/raw-table !data]]]]])
+  [:div.container-fluid.header
+   [:div.row
+    [:nav.navbar-default {:role "navigation"}
+     [:div.container-fluid
+      [:div.navbar-header
+       [:a.navbar-brand {:href "#"} "Stockport | SoNAR"]]
+      [:ul.nav.navbar-nav.navbar-right
+       [:li
+        [:button.btn.btn-default.navbar-btn
+         {:type "button"} "Log out"]]]]]]
+
+   [:div.row.body
+    [:div.column.container-criteria.col-md-3
+     [:div.column-title
+      "\n                    Search by\n                "]
+       [:div.panel.panel-default.criteria-box
+        [:div.panel-heading
+         [:form
+          [:button.btn.btn-default.pull-right {:type "button"} "Remove"]]
+         ]
+        [:div.panel-body
+         [:div.btn-group
+          [sc/search-control !app (h/default-handler !data)]]]]]
+
+    [:div.column.container-results.col-md-2
+      [:div.column-title
+       "\n                    Results\n                "]]
+    [:div.column.container-timeline.col-md-7
+     "Timeline"
+     [rt/raw-table !data]]]])
