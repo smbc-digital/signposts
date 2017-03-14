23513fa86860948a048649de244e41c12a41be1a
Richard Filippi <richard@rogerthemoose.co.uk>
2017-03-14 09:59:57 +0000

    rich - remove esproxy which has essentially moved into the visualise server side code

--- esproxy/test/gov/stockport/sonar/esproxy/config_tests.clj
+++
@@ -1,9 +0,0 @@
-(ns gov.stockport.sonar.esproxy.config-tests
-  (:require [midje.sweet :refer :all]
-            [gov.stockport.sonar.esproxy.config :as config]))
-
-(facts "about configuration"
-       (fact "it should load common config"
-             (keys (config/load-config)) => (contains #{:elastic-search})
-             (provided
-               (config/current-user-name) => "unknown-user")))
