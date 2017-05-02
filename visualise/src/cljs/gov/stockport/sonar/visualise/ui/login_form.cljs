b3c8de3443dd5a8bb8e42c999ecba146850e494a
Richard Filippi <richard@rogerthemoose.co.uk>
2017-05-02 13:13:51 +0100

    rich #ux - login with return keystroke on the login form

--- visualise/src/cljs/gov/stockport/sonar/visualise/ui/login_form.cljs
+++ visualise/src/cljs/gov/stockport/sonar/visualise/ui/login_form.cljs
@@ -4,23 +4,27 @@
 
 (def initial-state {:username "" :password ""})
 
+(defn- perform-search [!local]
+  (let [creds @!local]
+    (login creds)
+    (reset! !local initial-state)))
+
 (defn login-form []
   (let [!local (r/atom initial-state)]
     (fn []
       [:div.form
        [:div.form-group
         [:label "Username"
-        [:input.form-control.col-sm-12
-         {:type        :text
-          :value       (:username @!local)
-          :on-change   #(swap! !local assoc :username (-> % .-target .-value))}]]]
+         [:input.form-control.col-sm-12
+          {:type      :text
+           :value     (:username @!local)
+           :on-change #(swap! !local assoc :username (-> % .-target .-value))}]]]
        [:div.form-group
         [:label "Password"
-        [:input.form-control.col-sm-9
-         {:type        :password
-          :value       (:password @!local)
-          :on-change   #(swap! !local assoc :password (-> % .-target .-value))}]]]
+         [:input.form-control.col-sm-9
+          {:type      :password
+           :value     (:password @!local)
+           :on-change #(swap! !local assoc :password (-> % .-target .-value))
+           :on-key-up #(when (= 13 (-> % .-keyCode)) (perform-search !local))}]]]
        [:button.btn.btn-primary
-        {:on-click (fn [] (let [creds @!local]
-                            (login creds)
-                            (reset! !local initial-state)))} "Login"]])))
+        {:on-click (fn [] (perform-search !local))} "Login"]])))
