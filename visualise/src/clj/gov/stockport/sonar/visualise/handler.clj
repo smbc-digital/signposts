93b83f0c6e65152ab6bc6e546f9f8b33c4ca6b8e
Richard Filippi <richard@rogerthemoose.co.uk>
2017-05-22 12:16:53 +0100

    rich #ie - send browser a hint that the compatibility mode should be Edge when using IE

--- visualise/src/clj/gov/stockport/sonar/visualise/handler.clj
+++ visualise/src/clj/gov/stockport/sonar/visualise/handler.clj
@@ -24,6 +24,8 @@
    [:meta {:charset "utf-8"}]
    [:meta {:name    "viewport"
            :content "width=device-width, initial-scale=1"}]
+   [:meta {:http-equiv "X-UA-Compatible"
+           :content    "IE=Edge"}]
    (include-css (str (if (env :dev) "/css/site.css" "/css/site.min.css") "?v=" version)
                 "/css/font-awesome.min.css")])
 
