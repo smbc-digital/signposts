023e6ea08500a3f727faa0d767b4a01a7543d207
Richard Filippi <richard@rogerthemoose.co.uk>
2017-05-30 11:48:35 +0100

    rich #nuggets - show blank when no results

--- visualise/src/cljs/gov/stockport/sonar/visualise/ui/search/new_search_control.cljs
+++ visualise/src/cljs/gov/stockport/sonar/visualise/ui/search/new_search_control.cljs
@@ -12,8 +12,13 @@
                                                                   :idx (count (:terms local)))])
                                          (dissoc :val))))))))
 
+(defn remove-search-criteria [query-callback !local item]
+  (fn []
+    (let [terms (:terms (swap! !local update :terms (fn [terms] (filter (fn [term] (not (= item term))) terms))))]
+      (query-callback terms))))
+
 (defn nugget [!local query-callback {:keys [query val] :as item}]
-  [:div.nugget {:on-click #(query-callback (:terms (swap! !local update :terms (fn [terms] (filter (fn [term] (not (= item term))) terms)))))}
+  [:div.nugget {:on-click (remove-search-criteria query-callback !local item)}
    [:span.query (name query)]
    [:span.term val [:i.fa.fa-times]]])
 
@@ -21,25 +26,31 @@
   (let [!local (r/atom {:query (:target (first qcs/options))
                         :terms []})]
     (fn []
-      [:div.nuggets
-       [:select.input-sm
-        {:value     (:query @!local)
-         :autoFocus "autofocus"
-         :on-change #(swap! !local assoc :query (keyword (-> % .-target .-value)))}
-        (map
-          (fn [{:keys [target description]}]
-            ^{:key target}
-            [:option {:value target} description])
-          qcs/options)]
-       [:input.input-sm {:value       (:val @!local)
-                         :placeholder (get-in qcs/query-types [(:query @!local) :placeholder])
-                         :on-change   #(swap! !local assoc :val (-> % .-target .-value))
-                         :on-key-up   #(when (= 13 (-> % .-keyCode)) (add-search-criteria !local query-callback))}]
-
+      [:div.search-control.panel-body
+       [:p.info {:style {:color "#ffffff"}} "Add Search Term"]
+       [:div.nuggets
+        [:select.input-sm
+         {:value     (:query @!local)
+          :autoFocus "autofocus"
+          :on-change #(swap! !local assoc :query (keyword (-> % .-target .-value)))}
+         (map
+           (fn [{:keys [target description]}]
+             ^{:key target}
+             [:option {:value target} description])
+           qcs/options)]
+        [:input.input-sm {:value       (:val @!local)
+                          :placeholder (get-in qcs/query-types [(:query @!local) :placeholder])
+                          :on-change   #(swap! !local assoc :val (-> % .-target .-value))
+                          :on-key-up   #(when (= 13 (-> % .-keyCode)) (add-search-criteria !local query-callback))}]]
        `[:div.nuggets
          ~@(map (partial nugget !local query-callback) (:terms @!local))]])))
 
+(defn query-wrapper [handler]
+  (fn [terms]
+    (if (not-empty terms)
+      (search (qcs/extract-query-defs terms) handler)
+      (handler {}))))
+
 (defn new-search-control [handler]
-  (let [query-callback (fn [terms] (search (qcs/extract-query-defs terms) handler))]
-    [:div.search-control.panel-body
-     [search-criteria-control query-callback]]))
+  (let [query-callback (query-wrapper handler)]
+    [search-criteria-control query-callback]))
