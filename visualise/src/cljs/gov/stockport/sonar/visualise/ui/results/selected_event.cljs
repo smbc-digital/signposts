d09310f8c1f7c833dd892a0fdab6369ee82e1297
Richard Filippi <richard@rogerthemoose.co.uk>
2017-08-04 10:33:32 +0100

    rich #ui - remove some fields from the selected event view, and remove the signposting tab

--- visualise/src/cljs/gov/stockport/sonar/visualise/ui/results/selected_event.cljs
+++ visualise/src/cljs/gov/stockport/sonar/visualise/ui/results/selected_event.cljs
@@ -4,9 +4,9 @@
             [gov.stockport.sonar.visualise.util.fmt-help :as fh]
             [gov.stockport.sonar.visualise.ui.results.signposting :as s]))
 
-(def standard-keys [:name :dob :address :postcode :timestamp :score])
+(def standard-keys [:name :dob :address :postcode :timestamp])
 
-(def custom-formatter (f/formatter "dd MMM yyyy HH:mm:ss"))
+(def custom-formatter (f/formatter "EEE dd MMM yyyy"))
 
 (defn unparse-timestamp [event]
   (if-let [ts (:timestamp event)]
@@ -23,13 +23,13 @@
 
 (defn selected-kvs [event]
   (let [event-with-formatted-timestamp (-> event unparse-timestamp unparse-dob)
-        other-keys (sort (keys (apply dissoc (dissoc event :id :ingestion-timestamp) standard-keys)))]
+        other-keys (sort (keys (apply dissoc (dissoc event :id :ingestion-timestamp :score :event-type :event-source) standard-keys)))]
     (map
       (fn [k] [k (get event-with-formatted-timestamp k "")])
       (concat standard-keys other-keys))))
 
 (defn row [[k v]]
-  [:tr [:th (fh/label (name k))] [:td v]])
+  [:tr [:th (fh/label (name k))] [:td.col-12 v]])
 
 (defn rows [event]
   (map row (selected-kvs event)))
@@ -47,16 +47,17 @@
       (when (not-empty selected)
         [:div.selected-event.ml-2
          [:div.panel-group
-          [:div.panel.panel-default.event-details.col-sm-5
-           [:div.panel-heading (:event-type selected)]
-           [:div.panel-body
+          [:div.panel.panel-default.event-details.col-sm-11
+           [:div.panel-heading.my-2 (:event-type selected)]
+           [:div.panel-body.mb-2
             [:table.table-striped.table-condensed.results.selected-results
              `[:tbody
                ~@(rows selected)]
              ]]]
-          [:div.panel.panel-default.contact-panel.col-sm-7
-           [:div.panel-heading.contact-heading "SIGNPOST"]
-           [:div.panel-body
-            [:table.table-condensed.results.selected-results
-            `[:tbody
-              ~@(signpost-fields selected)]]]]]]))))
+          ;[:div.panel.panel-default.contact-panel.col-sm-7
+          ; [:div.panel-heading.contact-heading "SIGNPOST"]
+          ; [:div.panel-body
+          ;  [:table.table-condensed.results.selected-results
+          ;  `[:tbody
+          ;    ~@(signpost-fields selected)]]]]
+          ]]))))
