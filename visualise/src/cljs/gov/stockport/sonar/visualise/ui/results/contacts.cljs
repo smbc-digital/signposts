0d92a87769ec4809e1ef88169cb975ed7bf52955
Simon Estill <simon.estill@stockport.gov.uk>
2017-09-28 14:29:14 +0100

    refactored contacts code

--- visualise/src/cljs/gov/stockport/sonar/visualise/ui/results/contacts.cljs
+++ visualise/src/cljs/gov/stockport/sonar/visualise/ui/results/contacts.cljs
@@ -41,15 +41,15 @@
       (fn [k] [k (get event-with-formatted-timestamp k "")])
       (concat standard-keys other-keys))))
 
-(defn row [[k v]]
+(defn table-row [[k v]]
   [:tr [:th (fh/label (name k))] [:td v]])
 
-(defn rows [event]
+(defn event-details [event]
   [:div {:style {:border-top "solid 1px #ccc" :margin "15px 5px 10px 5px" :padding "10px 5px 10px 5px"}}
   [:h4  (:event-source event) " " [:span {:style {:font-weight "normal"}} (:event-type event)]]
   [:table
    [:tbody
-     (map row (selected-kvs event))
+     (map table-row (selected-kvs event))
   ]]])
 
 (defn toggle-events[events]
@@ -60,19 +60,21 @@
   )
 
 (defn list-events[events]
-    (let [events-list (:data events)]
+    (let [events-list  (:data events)]
+      (let [display-all! (r/atom false)]
+     (fn[]
      [:div.container-fluid
-      (if (:display-all events)
-        (map rows events-list)
-        (map rows (take 4 events-list))
+      (if (= display-all! true)
+        (map event-details events-list)
+        (map event-details (take 4 events-list))
         )
       (if (> (count events-list) 4)
       [:p {:style {:text-align "center" :font-size "2em"}}
        [:i.fa.fa-arrow-circle-down
-        {:on-click #((toggle-events events)) }
+        {:on-click (fn[e] (swap! display-all! true))}
         ]]
       )]
-     ))
+     ))))
 
 (defn list-people [people]
   (map (fn [[person-key  events]]
@@ -80,8 +82,8 @@
          [:div {:class (:color events)}
          [:h3 {:style {:text-align "center"}} (:name person-key) ]
              [:p {:style {:text-align "center"}}[:strong(count (:data events))] " contact data listed matches your search criteria"]
-            (list-events events)]))
-       (sort-by surname people)))
+            [list-events events]]))
+           people))
 
 (defn contact-history [!data]
   (let [results (:people @!data)]
