091d799750db45a6465e7d952e77008786bd3b46
Simon Estill <simon.estill@stockport.gov.uk>
2017-10-24 15:01:15 +0100

    styled placeholders in input

--- visualise/src/cljs/gov/stockport/sonar/visualise/ui/pages/login_page.cljs
+++ visualise/src/cljs/gov/stockport/sonar/visualise/ui/pages/login_page.cljs
@@ -6,14 +6,6 @@
 (defn login-page []
   [:div.login
    [busy/overlay]
-   ;[:div.container-fluid
-   ; {:style {:background-color "#1c3645" :color :white}}
-   ; [:div.row.align-items-center.py-1
-   ;  [:div.column.col-1
-   ;   [:div.row.justify-content-center
-   ;    [:i.fa.fa-map-signs.fa-2x]]]
-   ;  [:div.column.col-11
-   ;   [:span.h2 "SIGNPOSTS"]]]]
       [:div.login-form
      [:h4 [:i.fa.fa-map-signs.mr-2] [:br] "SIGNPOSTS"]
      [login-form]
