414aafe585f824602ebebc1bffc43c50aff2e11c
simon.estill <simon.estill@stockport.gov.uk>
2018-09-05 18:03:18 +0100

    SimonE fixedDOB bug

--- visualise/src/clj/gov/stockport/sonar/esproxy/es_search_history.clj
+++ visualise/src/clj/gov/stockport/sonar/esproxy/es_search_history.clj
@@ -14,8 +14,17 @@
   (defn query-search-history [user]
      (query db ["SELECT  Query FROM QueryLog WHERE User = ? ORDER BY Timestamp DESC LIMIT 10"  user]))
 
+
+  (defn parse-dob[query-field]
+    (if (= (:field-type query-field ) "date-of-birth")
+      {:query-type "dob" :search-term (:query query-field)}
+      {:query-type (:field-type query-field)  :search-term (:query query-field)}))
+
   (defn extract-query-field[query-field]
-    {:query-type (:field-name query-field) :search-term (:query query-field)})
+    (if(= (:field-name query-field) "dob")
+        (parse-dob query-field)
+        {:query-type (:field-name query-field) :search-term (:query query-field)}))
+
 
  (defn extract-query-fields[query]
     (let [query-fields (read-string (:query query))]
